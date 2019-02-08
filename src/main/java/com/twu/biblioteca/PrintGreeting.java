fb9cf8793391229343baac89549dac3bd900dae6
Rachael <sputnikboom@users.noreply.github.com>
2019-02-08 11:20:20 +0000

    1.1/Prints greeting message

---
+++ src/main/java/com/twu/biblioteca/PrintGreeting.java
@@ -0,0 +1,15 @@
+package com.twu.biblioteca;
+
+import java.io.PrintStream;
+
+public class PrintGreeting extends PrintMessage {
+    private String greeting = "Welcome to Biblioteca! Your one-stop shop for great book titles in Bangalore!";
+
+    public PrintGreeting(PrintStream printStream) {
+        super(printStream);
+    }
+
+    public void greet() {
+        print(this.greeting);
+    }
+}
